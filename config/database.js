24789f97b4f6ddacc5623a6d8d1b26ffd466e710
jainshruti429 <39043467+jainshruti429@users.noreply.github.com>
2018-06-11 12:09:47 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,12 +1,25 @@
 // config/database.js
 
 // configs to connect to the database
+
+//=================================USE THIS FOR COMPUTER===================================
+// module.exports = {
+//     'connection': {
+//       // local configs.
+//         'host' : 'localhost',
+//         'user': 'root',
+//         'password': 'root@SQL1'
+//     },
+//         'database': 'eqsAuction'
+// };
+
+//=================================USE THIS FOR SERVER===================================
 module.exports = {
     'connection': {
       // local configs.
-        'host' : 'localhost',
-        'user': 'root',
-        'password': 'root@SQL1'
+        'host' : 'mydbinstance.cq4rkzpvmufw.ap-south-1.rds.amazonaws.com',
+        'user': 'parag',
+        'password': 'equipshare'
     },
         'database': 'eqsAuction'
 };
