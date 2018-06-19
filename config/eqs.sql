419299f95c2de7ec699e36a32b9236a10da01e4c
jainshruti429 <jainshruti429@gmail.com>
2018-06-19 16:27:50 +0530

    database.js changed

--- config/eqs.sql
+++ config/eqs.sql
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `account`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/* SET character_set_client = utf8mb4 ;*/
 CREATE TABLE `account` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `password` varchar(255) DEFAULT NULL,
@@ -56,7 +56,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `admin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+ /*SET character_set_client = utf8mb4 ;*/
 CREATE TABLE `admin` (
   `id` int(5) DEFAULT NULL,
   `location` int(5) DEFAULT NULL,
@@ -79,7 +79,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `all_equipment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+ /*SET character_set_client = utf8mb4 ;*/
 CREATE TABLE `all_equipment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `brand` varchar(20) DEFAULT NULL,
@@ -127,7 +127,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `emails`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/*SET character_set_client = utf8mb4 ;*/
 CREATE TABLE `emails` (
   `email` varchar(60) DEFAULT NULL,
   `date` varchar(10) DEFAULT NULL,
@@ -135,7 +135,7 @@ CREATE TABLE `emails` (
   `sno` int(6) NOT NULL AUTO_INCREMENT,
   `remarks` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`sno`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -154,7 +154,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `equipment_type`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+ /*SET character_set_client = utf8mb4 ;*/
 CREATE TABLE `equipment_type` (
   `type_id` int(5) NOT NULL AUTO_INCREMENT,
   `category` varchar(50) DEFAULT NULL,
@@ -172,7 +172,7 @@ CREATE TABLE `equipment_type` (
   `subcategory` varchar(50) DEFAULT NULL,
   `brand` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`type_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -191,14 +191,14 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `featured`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+ /*SET character_set_client = utf8mb4 ;*/
 CREATE TABLE `featured` (
   `equip_id` int(11) NOT NULL,
   `display` int(1) NOT NULL,
   `start_date` varchar(10) DEFAULT NULL,
   `end_date` varchar(10) DEFAULT NULL,
   `views` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -217,11 +217,11 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `requests`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+ /*SET character_set_client = utf8mb4 ;*/
 CREATE TABLE `requests` (
   `equip_id` int(11) DEFAULT NULL,
   `applicant_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -240,11 +240,11 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `views`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+ /*SET character_set_client = utf8mb4 ;*/
 CREATE TABLE `views` (
   `equip_id` int(11) DEFAULT NULL,
   `viewer_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
